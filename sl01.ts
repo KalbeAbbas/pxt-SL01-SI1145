42a5e53e77e6d643c0e2b748f632b0aa793873b5
KalbeAbbas <kalbeabbas142@gmail.com>
2020-12-08 12:09:54 +0500

    Update sl01.ts

--- sl01.ts
+++ sl01.ts
@@ -1,9 +1,8 @@
-//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
-//% groups = '["New SL01", "Old SL01", "other"]'
+//%color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
 namespace SL01 {
 
-    //% blockId=device_show_number
-    //% block="show|number %v"
+    //%blockId=device_show_number
+    //%block="show|number %v"
     export function showNumber(v: number, interval: number = 150): void
     { }
 } 
